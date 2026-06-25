6c547be0aaa9e5042b8f547af8dc3aee24f4d07b
Dikshya Sitaula <dikshyasitaula13@gmail.com>
2026-06-25 15:55:35 +0545

    Nursery

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,4 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import Layout from './components/Layout';
-import Marketplace from './pages/Marketplace';
-import PlantDetail from './pages/PlantDetail';
-import Scan from './pages/Scan';
-import Rewards from './pages/Rewards';
-import Dashboard from './pages/Dashboard';
-import Landing from './pages/Landing';
-import Recommendation from './pages/Recommendation';
-import AboutUs from './pages/AboutUs';
-import Contact from './pages/Contact';
-import MobileBill from './pages/MobileBill';
 import NurseryAuth from './pages/NurseryAuth';
 import NurseryDashboard from './pages/NurseryDashboard';
 import NurseryProducts from './pages/NurseryProducts';
@@ -26,10 +15,10 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/about" element={<AboutUs />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/bill/:sessionId" element={<MobileBill />} />
+        {/* Default redirect */}
+        <Route path="/" element={<Navigate to="/nursery/signin" replace />} />
+
+        {/* Nursery Routes */}
         <Route path="/nursery/signin" element={<NurseryAuth />} />
         <Route path="/nursery/signup" element={<NurseryAuth />} />
         <Route path="/nursery/dashboard" element={<NurseryDashboard />} />
@@ -40,22 +29,15 @@ function App() {
         <Route path="/nursery/sales-report" element={<NurserySales />} />
         <Route path="/nursery/trending-products" element={<NurseryTrending />} />
         <Route path="/nursery/profile" element={<NurseryProfile />} />
+
+        {/* Admin Routes */}
         <Route path="/admin/dashboard" element={<AdminDashboard />} />
         <Route path="/admin/nurseries" element={<AdminNurseries />} />
         <Route path="/admin/products" element={<AdminProducts />} />
         <Route path="/admin/trending-products" element={<AdminTrending />} />
-        <Route element={<Layout />}>
-          <Route path="marketplace" element={<Marketplace />} />
-          <Route path="marketplace/:id" element={<PlantDetail />} />
-          <Route path="scan" element={<Scan />} />
-          <Route path="recommendation" element={<Recommendation />} />
-          <Route path="rewards" element={<Rewards />} />
-          <Route path="dashboard" element={<Dashboard />} />
-        </Route>
       </Routes>
     </BrowserRouter>
   );
 }
 
 export default App;
-   
